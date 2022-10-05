328cbc4862fe5bab7ee8145ac26479028fab8267
Pierre DM <pdaixmoreux@gmail.com>
2022-10-05 20:20:15 +0200

    feat: start ui

---
+++ zkrsa-ui/pages/verify.tsx
@@ -0,0 +1,11 @@
+import { NextComponentType } from "next";
+
+const Verify: NextComponentType = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default Verify;
