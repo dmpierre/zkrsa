06916d2c4accce02838237714f54fe3e32a4faf5
Pierre DM <pdaixmoreux@gmail.com>
2022-11-08 16:01:30 +0100

    make verifier state app global

--- zkrsa-ui/src/pages/index.tsx
+++ zkrsa-ui/src/pages/index.tsx
@@ -1,28 +1,33 @@
 import type { NextPage } from 'next';
 import { createContext, Dispatch, FunctionComponent, useState } from 'react';
-import { ButtonInitializeVerifier, ButtonGenerateProof } from '../components/Buttons';
+import { ButtonInitializeVerifier, ButtonGenerateProof, ButtonExportProof } from '../components/Buttons';
 import { InputHash, InputPublicKey, InputSignature, InputText } from '../components/Inputs';
 import { NavMenu, Title } from '../components/Navigation';
 
 
 interface StatusVKey {
   vkey: any | null;
+  vkeyState: string;
 }
-export const StatusVKey: FunctionComponent<StatusVKey> = ({ vkey }) => {
+export const StatusVKey: FunctionComponent<StatusVKey> = ({ vkey, vkeyState }) => {
 
   return (
-    <div className='mr-5 text-beige font-roboto-light-300'>
-      {vkey ? "Ready to generate proof" : "Verifier not initialized yet."}
+    <div className='mr-5 self-center text-beige font-roboto-light-300'>
+      {vkeyState}
     </div>
   );
 };
 
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
 
-const Home: NextPage<AppPageProps> = ({ vkeyVerifier, setvkeyVerifier, vkeyProof, setvkeyProof }) => {
+const Home: NextPage<AppPageProps> = ({ vkeyState, setvkeyState, vkeyVerifier, setvkeyVerifier, vkeyProof, setvkeyProof }) => {
   const [ hash, sethash ] = useState<null | string>(null);
   const [ signature, setsignature ] = useState<null | string>(null);
   const [ publicKey, setpublicKey ] = useState<null | string>(null);
   const [ proof, setproof ] = useState<null | string>(null);
+  const [ publicSignals, setpublicSignals ] = useState<null | any>(null);
   const [ compiledCircuit, setcompiledCircuit ] = useState(null);
 
   return (
@@ -30,12 +35,12 @@ const Home: NextPage<AppPageProps> = ({ vkeyVerifier, setvkeyVerifier, vkeyProof
       <Title></Title>
       <NavMenu></NavMenu>
       <div className='flex justify-center my-4'>
-        <StatusVKey vkey={vkeyProof}></StatusVKey>
-        <ButtonInitializeVerifier setvkeyVerifier={setvkeyVerifier} setvkeyProof={setvkeyProof}></ButtonInitializeVerifier>
+        <StatusVKey vkeyState={vkeyState} vkey={vkeyProof}></StatusVKey>
+        <ButtonInitializeVerifier setvkeyState={setvkeyState} setvkeyVerifier={setvkeyVerifier} setvkeyProof={setvkeyProof}></ButtonInitializeVerifier>
       </div>
-      <div className='flex flex-col items-center'>
+      <div className='mt-5 flex flex-col items-center'>
         <InputHash sethash={sethash}></InputHash>
-        <div className='my-4'>
+        <div className='my-10'>
           <InputSignature setsignature={setsignature}></InputSignature>
         </div>
         <InputPublicKey setpublicKey={setpublicKey}></InputPublicKey>
@@ -45,8 +50,12 @@ const Home: NextPage<AppPageProps> = ({ vkeyVerifier, setvkeyVerifier, vkeyProof
           vkeyVerifier={vkeyVerifier}
           vkeyProof={vkeyProof}
           setcompiledCircuit={setcompiledCircuit}
+          setpublicSignals={setpublicSignals}
           hash={hash} signature={signature}
           publicKey={publicKey} setproof={setproof}></ButtonGenerateProof>
+      </div>
+      <div className='my-4 flex flex-col items-center'>
+        <ButtonExportProof publicSignals={publicSignals} proof={proof}></ButtonExportProof>
       </div>
     </div>
   );
--- zkrsa-ui/src/pages/verify.tsx
+++ zkrsa-ui/src/pages/verify.tsx
@@ -1,18 +1,17 @@
 import { NextPage } from "next";
 import { StatusVKey } from ".";
-import { AppPageProps } from "..";
 import { ButtonInitializeVerifier } from "../components/Buttons";
 import { Title, NavMenu } from "../components/Navigation";
 
-const Verify: NextPage<AppPageProps> = ({ vkeyVerifier, setvkeyVerifier, vkeyProof, setvkeyProof}) => {
+const Verify: NextPage<AppPageProps> = ({ vkeyState, setvkeyState, vkeyVerifier, setvkeyVerifier, vkeyProof, setvkeyProof}) => {
     console.log(vkeyVerifier);
     return (
         <div>
             <Title></Title>
             <NavMenu></NavMenu>
             <div className='flex justify-center my-4'>
-                <StatusVKey vkey={vkeyProof}></StatusVKey>
-                <ButtonInitializeVerifier setvkeyVerifier={setvkeyVerifier} setvkeyProof={setvkeyProof}></ButtonInitializeVerifier>
+                <StatusVKey vkeyState={vkeyState} vkey={vkeyProof}></StatusVKey>
+                <ButtonInitializeVerifier setvkeyState={setvkeyState} setvkeyVerifier={setvkeyVerifier} setvkeyProof={setvkeyProof}></ButtonInitializeVerifier>
             </div>
 
         </div>
