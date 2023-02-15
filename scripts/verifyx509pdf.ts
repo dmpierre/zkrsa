faddc6e8531b0ead4efc30bb169666c1aff4536b
Pierre DM <pdaixmoreux@gmail.com>
2023-02-15 10:33:37 +0100

    add different examples for sig verification

--- scripts/verifyx509pdf.ts
+++ scripts/verifyx509pdf.ts
@@ -1,11 +1,21 @@
-import { createVerify, X509Certificate } from "crypto";
+import {
+  createVerify,
+  createHash,
+  X509Certificate,
+  publicDecrypt,
+} from "crypto";
 import fs from "fs";
 import { asn1 } from "node-forge";
+import * as forge from "node-forge";
 import { exit } from "process";
+import assert from "assert";
 //@ts-ignore
 const { extractSignature } = require("node-signpdf");
+const bigInt = require("big-integer");
 
 const main = () => {
+  const rsa = forge.pki.rsa;
+
   const pdf = fs.readFileSync("./adhaar.pdf");
   let pem = fs.readFileSync("./cert.pem");
 
@@ -13,13 +23,37 @@ const main = () => {
   const { ByteRange, signedData, signature } = extractSignature(pdf);
   const x509 = new X509Certificate(pem);
   const signatureASN1 = asn1.fromDer(signature).value as string;
+  const pubKey = x509.publicKey.export({ format: "jwk" });
+
+  // ensure signature is of correct format
+  const signatureLength = Buffer.from(signatureASN1, "binary").length;
+  assert(
+    signatureLength == 256,
+    `Signature is of incorrect length: ${signatureLength}; should be 256.`
+  );
 
   // verify signature
   const verifyAlgo = createVerify("RSA-SHA1");
   verifyAlgo.update(signedData);
   const result = verifyAlgo.verify(x509.publicKey, signatureASN1, "binary");
 
-  console.log(`Signature verified: ${result}`);
+  assert(result);
+
+  // manual check - additional example
+  const sha1 = createHash("SHA1");
+  sha1.update(signedData);
+  const digest = sha1.digest();
+  const decrypted = publicDecrypt(
+    x509.publicKey,
+    Buffer.from(signatureASN1, "binary")
+  );
+  /* 
+    signed data is: SHA1_ID || M
+    where SHA1_ID is (0x)30 21 30 09 06 05 2b 0e 03 02 1a 05 00 04 14 
+    https://www.rfc-editor.org/rfc/rfc8017#section-9.2 (p. 46) 
+  */
+  assert(Buffer.compare(digest, decrypted.subarray(15, decrypted.length)) == 0);
+
   return 0;
 };
 
