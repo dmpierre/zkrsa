10b586e97e25c32cd358e763f19dd88e175864a1
Pierre DM <pdaixmoreux@gmail.com>
2023-02-23 13:57:08 +0100

    add circuit scripts for rsa-sha1

--- circom-rsa-verify/scripts/generateKeys.ts
+++ circom-rsa-verify/scripts/generateKeys.ts
@@ -1,36 +1,44 @@
 //@ts-ignore
 import snarkjs from "snarkjs";
-import { readFileSync } from "fs";
 import {
   stringifyBigInts,
-  unstringifyBigInts,
   //@ts-ignore
 } from "snarkjs/src/stringifybigint.js";
 import fs from "fs";
+//@ts-ignore
+import compiler from "circom";
+import path from "path";
+import { exit } from "process";
 
-const circuitName = process.argv[2];
-console.log(circuitName)
+const main = async () => {
+  const circuitName = process.argv[2];
 
-console.log("Loading circuit..");
-const circuitDef = JSON.parse(readFileSync(`./circuits/${circuitName}.json`, "utf-8"));
+  console.log("Compiling circuit..");
 
-console.log("Instantiating circuit...");
-const circuit = new snarkjs.Circuit(circuitDef);
+  const circuitDef = await compiler(
+    path.join(__dirname, "../test/circuits/", `${circuitName}.circom`)
+  );
 
-console.log("Starting setup..");
-const setup = snarkjs.original.setup(circuit);
+  console.log("Instantiating circuit...");
+  const circuit = new snarkjs.Circuit(circuitDef);
 
-console.log("Writing vk_proof");
-fs.writeFileSync(
-  `./vkeys/${circuitName}.vk_proof`,
-  JSON.stringify(stringifyBigInts(setup.vk_proof)),
-  "utf8"
-);
+  console.log("Starting setup..");
+  const setup = snarkjs.original.setup(circuit);
 
-console.log("Writing vk_verifier");
+  console.log("Writing vk_proof");
+  fs.writeFileSync(
+    `./vkeys/${circuitName}.vk_proof`,
+    JSON.stringify(stringifyBigInts(setup.vk_proof)),
+    "utf8"
+  );
 
-fs.writeFileSync(
-  `./vkeys/${circuitName}.vk_verifier`,
-  JSON.stringify(stringifyBigInts(setup.vk_verifier)),
-  "utf8"
-);
+  console.log("Writing vk_verifier");
+
+  fs.writeFileSync(
+    `./vkeys/${circuitName}.vk_verifier`,
+    JSON.stringify(stringifyBigInts(setup.vk_verifier)),
+    "utf8"
+  );
+};
+
+main().then(() => exit(1));
